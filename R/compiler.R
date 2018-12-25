eeec2c00929ddf87f6bb4100b2a84d804c483d7a
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2018-12-25 08:47:58 +0000

    Ensure full compilation of upstream reports.

--- R/compiler.R
+++ R/compiler.R
@@ -1,7 +1,7 @@
 .compiler <- function(target) 
 # Compiles all dependent workflows in an enslaved R session.
 {
-    if (!file.exists(paste0(target, "_cache"))) {
+    if (!file.exists(paste0(target, ".html"))) {
         script <- paste0(target, ".Rmd")
         rcmd <- file.path(R.home("bin"), "R")
         code <- system2(rcmd, c("--no-save", "--slave", "-e", sprintf("'rmarkdown::render(\"%s\")'", script)))
