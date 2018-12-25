1c5bd81e8b3bd0003cfe5a106b6e013950e07f2d
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2018-12-25 06:07:49 +0000

    Stop master script if upstream script fails.

--- R/compiler.R
+++ R/compiler.R
@@ -2,8 +2,12 @@
 # Compiles all dependent workflows in an enslaved R session.
 {
     if (!file.exists(paste0(target, "_cache"))) {
+        script <- paste0(target, ".Rmd")
         rcmd <- file.path(R.home("bin"), "R")
-        system2(rcmd, c("--no-save", "--slave", "-e", sprintf("'rmarkdown::render(\"%s\")'", paste0(target, ".Rmd"))))
+        code <- system2(rcmd, c("--no-save", "--slave", "-e", sprintf("'rmarkdown::render(\"%s\")'", script)))
+        if (code) {
+            stop(sprintf("could not run '%s'", script))
+        }
     }
     invisible(NULL)
 }
